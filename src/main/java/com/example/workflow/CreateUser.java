250d63988102980b822e8341c8ac504cffd1dc09
iNatashka99 <iNatashka99@yandex.ru>
2023-06-06 15:15:56 +0300

    go

---
+++ src/main/java/com/example/workflow/CreateUser.java
@@ -0,0 +1,14 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateUser implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        System.out.printf("[%s] Пользователь %s зарегистрирован\n", CreateUser.class.getName(),
+                delegateExecution.getVariable("username"));
+    }
+}
---
+++ src/main/java/com/example/workflow/Notification.java
@@ -0,0 +1,20 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Notification implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        if ((Boolean) delegateExecution.getVariable("decision")) {
+            System.out.printf("[%s] %s, здравствуйте!\n",
+                    Notification.class.getName(), delegateExecution.getVariable("username"));
+        } else {
+            System.out.printf("[%s] %s, Вам отказано в регистрации!\n", Notification.class.getName(),
+                    delegateExecution.getVariable("username"));
+        }
+    }
+}
